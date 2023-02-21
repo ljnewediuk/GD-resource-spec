98d1ed675148bb640cce7586600e33692414cff0
ljnewediuk <j.newedi@gmail.com>
2023-02-21 15:23:00 -0600

    update function for movement states

--- functions/GetMovementStates.R
+++ functions/GetMovementStates.R
@@ -15,16 +15,53 @@ movement_states <- function(study_id, mv_creds) {
     
     # Subset out individual from dataset and print name for warnings/errors
     print(j)
-    data_j <- df %>%
+    dat_j <- df %>%
       filter(individual.id == j)
     
-    # Screen out individuals with fewer than 5 locations
-    if(nrow(data_j) < 5) next
+    # If individual has < 6 points total, move to next
+    if(nrow(dat_j) < 6) next
+    
+    # Filter timesteps too far outside regular timestep interval ====
+    check_times_j <- dat_j  %>%
+      mutate(diff_time = c(difftime(tail(timestamp, -1), head(timestamp, -1)), 0)) %>%
+      mutate(diff = as.numeric(diff_time),
+             time_units = units(diff_time)) %>%
+      dplyr::select(! diff_time) %>%
+      # Remove the last row in the group ( = 0 hrs)
+      slice(1: n() -1) %>%
+      group_by(diff) %>%
+      mutate(n = n()) %>%
+      ungroup()
+    
+    # Get the most frequent time interval
+    reg_time <- check_times_j %>%
+      filter(n == max(check_times_j$n)) %>%
+      distinct(diff) 
+    
+    reg_time <- max(reg_time)
+    
+    # For all rows in check_times, if fix is outside of tolerance, assign new track segment value
+    timeblocks_j <- check_times_j %>%
+      mutate(reg_time) %>%
+      mutate(segment = cumsum(diff > reg_time * 1.3 | diff < reg_time * 0.7))
+
+    # Grab only the segments with the most continuous points
+    which_most_k <- timeblocks_j %>% 
+      group_by(segment) %>%
+      summarize(n_pts = n()) %>%
+      filter(n_pts %in% max(n_pts))
+    
+    filtered_timeblocks <- timeblocks_j %>%
+      filter(segment %in% which_most_k$segment)
+    
+    # Screen individual if segments have fewer than 5 locations to calculate movement
+    if(any(which_most_k$n_pts < 5)) next
     
     # Prep data for HMM
-    prepped_j <- data_j %>%
-      dplyr::select(location.long, location.lat, individual.id) %>%
-      rename('ID' = individual.id) %>%
+    prepped_j <- filtered_timeblocks %>%
+      dplyr::select(location.long, location.lat, individual.id, segment) %>%
+      # Make each segment a separate "ID" for modelling
+      rename('ID' = segment) %>%
       prepData(type = 'LL', coordNames = c('location.long', 'location.lat'))
     
     # Define parameters
@@ -47,23 +84,24 @@ movement_states <- function(study_id, mv_creds) {
         cat("Can't find appropriate starting values", "\n")
         break
       } 
-      
-      stepMean0 <- runif(2, 
+      tryCatch({
+        stepMean0 <- runif(2, 
+                           min = c(quantile(prepped_j$step, probs = 0.1, na.rm = T),
+                                   quantile(prepped_j$step, probs = 0.5, na.rm = T)),
+                           max = c(quantile(prepped_j$step, probs = 0.5, na.rm = T),
+                                   quantile(prepped_j$step, probs = 0.9, na.rm = T)))
+        stepSD0 <- runif(2, 
                          min = c(quantile(prepped_j$step, probs = 0.1, na.rm = T),
                                  quantile(prepped_j$step, probs = 0.5, na.rm = T)),
                          max = c(quantile(prepped_j$step, probs = 0.5, na.rm = T),
                                  quantile(prepped_j$step, probs = 0.9, na.rm = T)))
-      stepSD0 <- runif(2, 
-                       min = c(quantile(prepped_j$step, probs = 0.1, na.rm = T),
-                               quantile(prepped_j$step, probs = 0.5, na.rm = T)),
-                       max = c(quantile(prepped_j$step, probs = 0.5, na.rm = T),
-                               quantile(prepped_j$step, probs = 0.9, na.rm = T)))
-      angleMean0 <- c(0, 0)
-      angleCon0 <- runif(2,
-                        min = c(quantile(prepped_j$angle, probs = 0.5, na.rm = T),
-                                quantile(prepped_j$angle, probs = 0.7, na.rm = T)),
-                        max = c(quantile(prepped_j$angle, probs = 0.7, na.rm = T),
-                                quantile(prepped_j$angle, probs = 0.9, na.rm = T)))
+        angleMean0 <- c(0, 0)
+        angleCon0 <- runif(2,
+                           min = c(quantile(prepped_j$angle, probs = 0.5, na.rm = T),
+                                   quantile(prepped_j$angle, probs = 0.7, na.rm = T)),
+                           max = c(quantile(prepped_j$angle, probs = 0.7, na.rm = T),
+                                   quantile(prepped_j$angle, probs = 0.9, na.rm = T)))
+      })
       
       if(length(whichzero) == 0) {
         stepPar0 <- c(stepMean0, stepSD0)
@@ -73,6 +111,8 @@ movement_states <- function(study_id, mv_creds) {
       
       anglePar0 <- c(angleMean0, angleCon0)
       
+      if(all(is.nan(angleCon0)) | all(is.nan(angleMean0))) next
+      
       if(any(c(angleCon0) < 0)) {
         rep_no <- rep_no - 1
         next
@@ -80,18 +120,17 @@ movement_states <- function(study_id, mv_creds) {
       
       tryCatch({
         hmm_m <- fitHMM(prepped_j, nbStates = 2, stepPar0 = stepPar0, anglePar0 = anglePar0)
+        mod_list[[rep_no]] <- hmm_m
       }, error=function(e){
         rep_no <- rep_no - 1
         cat("ERROR :",conditionMessage(e), "\n")
       })
       
-      mod_list[[rep_no]] <- hmm_m
+      
       
       if(rep_no >= 25) break
       
-      }
-      
-    
+    }
     
     # Move to next animal if can't find a model
     if(length(mod_list) == 0) next
@@ -103,9 +142,20 @@ movement_states <- function(study_id, mv_creds) {
     # Get model with min negative log likelihood
     best_mod <- mod_list[[which(allnllk == min(allnllk))[1]]]
     
+    # If can't estimate step or angle parameters or model, move to next
+    if(is.null(best_mod)) next
+    if(best_mod$mle$anglePar[[1]] == 0 | best_mod$mle$stepPar[[1]] == 0) next
+    
+    # Catch error in viterbi, move to next if error
+    tryCatch({
+      test_viterbi <- factor(viterbi(best_mod))
+    }, error=function(e){
+      next
+    })
+    
     # Add most likely state sequence to data (index of min log likelihood),
     # plus movement parameters
-    state_j <- data_j %>%
+    state_j <- filtered_timeblocks %>%
       mutate(state = factor(viterbi(best_mod))) %>%
       mutate(sl_mean = ifelse(state == 1, 
                               best_mod$mle$stepPar[[1]], best_mod$mle$stepPar[[3]]),
@@ -114,12 +164,13 @@ movement_states <- function(study_id, mv_creds) {
              ta_mean = ifelse(state == 1, 
                               best_mod$mle$anglePar[[1]], best_mod$mle$anglePar[[3]]),
              ta_conc = ifelse(state == 1, 
-                            best_mod$mle$anglePar[[2]], best_mod$mle$anglePar[[4]]))
+                              best_mod$mle$anglePar[[2]], best_mod$mle$anglePar[[4]]))
     
     # *** Add mean turn angles and step lengths for each state (should be high TA/
     #     low step lengths for foraging behaviour)
     
     state_data <- rbind(state_data, state_j)
+    
     
   }
   
@@ -127,5 +178,5 @@ movement_states <- function(study_id, mv_creds) {
   
 }
 
-
-
+study_id <- 259966228
+j <- 277058475
