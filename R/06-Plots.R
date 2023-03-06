106add5466fd45f75d642431798dcc2c033a9159
ljnewediuk <j.newedi@gmail.com>
2023-03-06 17:29:42 -0600

    Adjust metrics

--- R/06-Plots.R
+++ R/06-Plots.R
@@ -1,4 +1,6 @@
 
+library(tidyverse)
+
 # 06 Plots ====
 # Plot movement on environment, GD on environment, GD on movement
 
@@ -16,48 +18,63 @@ panther <- read.csv('input/imputation_phylo_1.csv') %>%
 # Combine data
 all_dat <- movement_summary %>%
   left_join(lc_summary) %>%
-  mutate(speed = ifelse(time_units == 'secs', speed*3600, speed),
+  mutate(intensity_use = cumulat_dist/(hr_area^2),
+         speed = ifelse(time_units == 'secs', speed*3600, speed),
          speed = ifelse(time_units == 'mins', speed*60, speed),
-         speed = ifelse(time_units == 'days', speed/24, speed),
-         hr_area = ifelse(time_units == 'secs', hr_area*3600, hr_area),
-         hr_area = ifelse(time_units == 'mins', hr_area*60, hr_area),
-         hr_area = ifelse(time_units == 'days', hr_area/24, hr_area)) %>%
+         speed = ifelse(time_units == 'days', speed/24, speed)) %>%
   mutate(log_speed = log(abs(speed+.001))) %>%
   mutate(log_hr_area = log(hr_area+.001)) %>%
-  mutate(intensity_use = speed/(hr_area^2)) %>%
+  mutate(log_intensity = log(intensity_use+.001)) %>%
   filter(! taxon %in% 'nyctalus_noctula') %>%
   pivot_longer(cols = c(hab_contrast_sc, simpsons_D_sc, pet_sc), names_to = 'env_char') %>%
   mutate(env_char = factor(env_char),
          study = factor(study)) %>%
-  left_join(panther)
+  left_join(panther) %>%
+  na.omit()
   # NOTE: below lines depend on having checked which species are in common
   # between movement and genetic diversity data in 03-GD-Movement-Intersect
   # mutate(env_char = factor(env_char),
   #        taxon = ifelse(! taxon %in% tolower(mv_gd_mtch), 'other', taxon)) %>%
   # filter(! taxon  == 'other')
 
+
 # Model lm
-mod <- glmmTMB::glmmTMB(log_speed ~ value +  (value | taxon), data = all_dat[all_dat$env_char == 'simpsons_D_sc',])
+glmmTMB::glmmTMB(log_speed ~ value +  (value | taxon), data = all_dat[all_dat$env_char == 'simpsons_D_sc',]) %>% 
+  summary()
+
+glmmTMB::glmmTMB(log_intensity ~ value +  (value | taxon), data = all_dat[all_dat$env_char == 'simpsons_D_sc',]) %>% 
+  summary()
+
+glmmTMB::glmmTMB(log_speed ~ value +  (value | taxon), data = all_dat[all_dat$env_char == 'pet_sc',]) %>% 
+  summary()
+
+glmmTMB::glmmTMB(log_intensity ~ value +  (value | taxon), data = all_dat[all_dat$env_char == 'pet_sc',]) %>% 
+  summary()
 
 # Effects on speed
 ggplot(all_dat, aes(x = value, y = log_speed, colour = taxon)) + 
   geom_point() + 
-  facet_wrap(~env_char, scales = 'free_x') + 
-  geom_smooth(method = 'lm')
+  scale_colour_viridis_d() +
+  facet_wrap(~env_char, scales = 'free_x') +
+  ylab('Log speed (m/h)') + xlab('Scaled value') +
+  theme(legend.position = 'none') +
+  ylab('Log home range area (m2)') + xlab('Scaled value')
+
 ggplot(all_dat, aes(x = log(adult_mass_g), y = log_speed)) + 
   geom_point() + 
   facet_wrap(~env_char, scales = 'free_x') + 
   geom_smooth(method = 'lm')
 
 # Effects on MCP area
-ggplot(all_dat, aes(x = value, y = log_hr_area)) + 
+ggplot(all_dat, aes(x = value, y = log_hr_area, colour = taxon)) + 
   geom_point() + 
-  facet_wrap(~env_char, scales = 'free_x') + 
-  geom_smooth(method = 'lm')
+  scale_colour_viridis_d() +
+  facet_wrap(~env_char, scales = 'free_x') +
+  theme(legend.position = 'none') +
+  ylab('Log home range area (m2)') + xlab('Scaled value')
 
 # Effects on use intensity
-ggplot(all_dat, aes(x = value, y = log(intensity_use))) + 
+ggplot(all_dat, aes(x = value, y = log_intensity)) + 
   geom_point() +
   facet_wrap(~env_char, scales = 'free')
-
 
