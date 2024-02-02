7732b7af7c4a25e9c89f5e53a3101dca1bf4901b
ljnewediuk <j.newedi@gmail.com>
2024-02-01 19:02:24 -0600

    rename plots

--- R/09-Plots.R
+++ R/09-Plots.R
@@ -1,8 +1,9 @@
 
 library(tidyverse)
 library(cowplot)
+library(png)
 
-# 06 Plots ====
+# 09 Main Plots ====
 # Orchard plots of movement ~ environment
 # Scatterplot of Fst ~ environment
 
@@ -10,11 +11,15 @@ library(cowplot)
 rand_vars <- readRDS('output/movement_rand_draws.rds') %>%
   mutate(response = factor(response, 
                            levels = c('speed', 'hr'), 
-                           labels = c('Speed', 'Home range size')))
+                           labels = c('Speed', 'Home range size'))) %>%
+  # ** Remove PET!
+  filter(! var == 'Potential evapotranspiration')
 fixed_vars <- readRDS('output/movement_fixed_draws.rds') %>%
   mutate(response = factor(response, 
                            levels = c('speed', 'hr'), 
-                           labels = c('Speed', 'Home range size')))
+                           labels = c('Speed', 'Home range size')))  %>%
+  # ** Remove PET!
+  filter(! var == 'Potential evapotranspiration')
 fst_dat <- readRDS('output/prepped_fst_data.rds')
 
 # Get mean of dispersal, hr, and fst for unscaling
@@ -26,19 +31,19 @@ mean_fst <- mean(fst_dat$global_fst, na.rm = T)
 sd_fst <- sd(fst_dat$global_fst, na.rm = T)
 
 fst_hr_draws <- readRDS('output/fst_hr_draws.rds') %>%
-  mutate(fst = .prediction * sd_fst + mean_fst)
+  mutate(fst = .epred * sd_fst + mean_fst)
 
 fst_disp_draws <- readRDS('output/fst_disp_draws.rds') %>%
   mutate(dispersal_km = dispersal_km_sc * sd_disp + mean_disp,
-         fst = .prediction * sd_fst + mean_fst)
+         fst = .epred * sd_fst + mean_fst)
 
-# Orchard plot
+# Orchard plot for main text
 ggplot() + 
   geom_hline(yintercept = 0) + 
   geom_jitter(data = rand_vars, 
               aes(x = var, y = mean_slope, colour = lifestyle, size = log(adult_mass_g)),
-              alpha = 0.3) + 
-  scale_color_manual(values = c('#ff3442', '#009ce9')) +
+              alpha = 0.4) + 
+  scale_color_manual(values = c('#009ce9', '#B29784')) +
   theme(axis.title.y = element_blank(),
         legend.title = element_blank()) +
   geom_linerange(data = fixed_vars, 
@@ -66,10 +71,9 @@ ggsave('orchard_plot.tiff', last_plot(), path = 'figures/', device = 'tiff', dpi
 
 # Panel scatterplots for Fst models
 disp_plot <- ggplot() +
-  ggdist::stat_lineribbon(data = fst_disp_draws, aes(x = dispersal_km, y = fst), 
-                          .width = c(0.95, 0.8, 0.5), alpha = 0.6) +
   geom_point(data = fst_dat, aes(x = dispersal_km, y = global_fst), alpha = 0.5) +
-  scale_fill_brewer(palette = 'Blues') +
+  ggdist::stat_lineribbon(data = fst_disp_draws, aes(x = dispersal_km, y = fst), 
+                          .width = 0.95, colour = 'black', fill = '#33333340', linewidth = 0.5) +
   theme(panel.background = element_rect(colour = 'white', fill = 'white', linewidth = 1),
         axis.line.x = element_line(colour = 'black'),
         axis.line.y = element_line(colour = 'black'),
@@ -80,13 +84,13 @@ disp_plot <- ggplot() +
         legend.position = 'none',
         panel.grid = element_blank(),
         plot.margin = unit(c(0.5, 0.5, 1, 1), 'cm')) +
-  xlab('Maximum dispersal distance (km)') + ylab(bquote('Global '~italic(F)[ST]))
+  xlab('Maximum dispersal distance (km)') + ylab(bquote('Population-specific '~italic(F)[ST]))
 
 hr_plot <- ggplot() +
-  ggdist::stat_lineribbon(data = fst_hr_draws, aes(x = log_hr_km2, y = fst), 
-                          .width = c(0.95, 0.8, 0.5), alpha = 0.6) +
   geom_point(data = fst_dat, aes(x = log_hr_km2, y = global_fst), alpha = 0.5) +
-  scale_fill_brewer(palette = 'Reds') +
+  ggdist::stat_lineribbon(data = fst_hr_draws, aes(x = log_hr_km2, y = fst), 
+                          .width = 0.95, colour = 'black', fill = '#33333340', linewidth = 0.5) +
+  scale_fill_brewer(palette = 'Greys') +
   theme(panel.background = element_rect(colour = 'white', fill = 'white', linewidth = 1),
         axis.line.x = element_line(colour = 'black'),
         axis.line.y = element_line(colour = 'black'),
